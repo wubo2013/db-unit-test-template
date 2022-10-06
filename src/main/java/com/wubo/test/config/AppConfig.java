2f7be88abdd9f73d96554590409c2fc138fbdc26
nowave <cumt2003@126.com>
2022-10-06 21:38:45 +0800

    初始提交

---
+++ src/main/java/com/wubo/test/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.wubo.test.config;
+
+import com.wubo.test.rpc.ITestFeignService;
+import feign.Feign;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
