03948c645fa81bd0ffcc7816a714a986cf185c84
wubo2013 <cumt2003@126.com>
2022-10-12 23:36:04 +0800

    利用h2构建测试

--- src/main/java/com/wubo/test/service/impl/StudentServiceImpl.java
+++ src/main/java/com/wubo/test/service/impl/StudentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.wubo.test.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wubo.test.dao.StudentMapper;
 import com.wubo.test.model.entity.StudentEntity;
@@ -14,6 +15,12 @@ public class StudentServiceImpl extends ServiceImpl<StudentMapper , StudentEntit
 
     @Override
     public long testInsert(StudentEntity entity) {
+        String sqlSelect = new LambdaQueryWrapper<StudentEntity>()
+                .select(StudentEntity::getCname ,StudentEntity::getTname)
+                .eq(StudentEntity::getCname, "31231")
+                .getTargetSql();
+        log.info(sqlSelect);
+
         return this.baseMapper.insert(entity);
     }
 }
--- src/test/java/com/wubo/test/TestStaticVar.java
+++ src/test/java/com/wubo/test/TestStaticVar.java
@@ -1,11 +1,17 @@
 package com.wubo.test;
 
+import com.baomidou.mybatisplus.core.toolkit.LambdaUtils;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
 import com.wubo.test.constant.AppConstant;
 import com.wubo.test.model.entity.TeacherEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Method;
+import java.util.function.Function;
 
 @Slf4j
 public class TestStaticVar extends TestApplicationTests{
@@ -24,7 +30,24 @@ public class TestStaticVar extends TestApplicationTests{
     }
 
     @Test
-    public void testStaticVar(){
+    public void testStaticVar() throws Exception {
         log.info("teacherEntity tname->{}" , teacherEntity.getTname());
+    }
+
+    private static <T, R> java.lang.invoke.SerializedLambda doSFunction(SFunction<T, R> func) throws Exception {
+        // 直接调用writeReplace
+        Method writeReplace = func.getClass().getDeclaredMethod("writeReplace");
+        writeReplace.setAccessible(true);
+        //反射调用
+        Object sl = writeReplace.invoke(func);
+        java.lang.invoke.SerializedLambda serializedLambda = (java.lang.invoke.SerializedLambda) sl;
+        return serializedLambda;
+    }
+
+    @Test
+    public void testGetLambdaMethod() throws Exception {
+        SerializedLambda serializedLambda = doSFunction(TeacherEntity::getTname);
+        log.info("method-> {}" , serializedLambda.getImplMethodName());
+        log.info("class->{}" , serializedLambda.getImplClass());
     }
 }
